70d5dd1ff7c7b5f7106b8e6d90fb9fdbb5157d8c
Olehmuz <olehmuz87@gmail.com>
2022-10-30 00:54:15 +0300

    lab2

--- index.js
+++ index.js
@@ -1,172 +1,141 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-var Student = /** @class */ (function () {
-    function Student(full_name, address, phone_number, email, student_number, average_mark, courses) {
-        this.full_name = full_name;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var PersonalInfo = /** @class */ (function () {
+    function PersonalInfo(id, fullName, address, phoneNumber, email, position, rank, salary) {
+        this.id = id;
+        this.fullName = fullName;
         this.address = address;
         this.email = email;
-        this.student_number = student_number;
-        this.phone_number = phone_number;
-        this.average_mark = average_mark;
-        this.courses = courses;
-    }
-    Student.prototype.can_enroll = function (course) {
-        if (course.students.includes(this) && this.courses.includes(course)) {
-            console.log("".concat(this.full_name, " may enroll."));
-            return true;
-        }
-        console.log("".concat(this.full_name, " can't enroll."));
-        return false;
-    };
-    ;
-    Student.prototype.taken_courses = function () {
-        console.log("Student courses: ".concat(this.courses));
-        return this.courses;
-    };
-    ;
-    Student.prototype.enroll = function (course) {
-        if (this.courses.includes(course)) {
-            console.log("".concat(this.full_name, " currently enroll this course"));
-        }
-        else {
-            this.courses = __spreadArray(__spreadArray([], this.courses, true), [course], false);
-            console.log("".concat(this.full_name, " enrolled this course"));
-        }
-    };
-    ;
-    Student.prototype.unenroll = function (course) {
-        if (!this.courses.includes(course)) {
-            console.log("".concat(this.full_name, " currently unenrolled this course"));
-        }
-        else {
-            this.courses = this.courses.filter(function (el) { return el.title != course.title; });
-            console.log("".concat(this.full_name, " unenrolled this course"));
-        }
-    };
-    ;
-    return Student;
-}());
-var Course = /** @class */ (function () {
-    function Course(title, start_date, end_date, description, lectures, assignments, limit, students) {
-        this.title = title;
-        this.start_date = start_date;
-        this.end_date = end_date;
-        this.description = description;
-        this.lectures = lectures;
-        this.assignments = assignments;
-        this.limit = limit;
-        this.students = students;
-    }
-    Course.prototype.add_student = function (student) {
-        if (this.students.length + 1 <= this.limit) {
-            if (this.students.includes(student)) {
-                console.log("This student already has been added");
-            }
-            else {
-                this.students = __spreadArray(__spreadArray([], this.students, true), [student], false);
-            }
-        }
-        else {
-            console.log("The student limit for this course has been exceeded. ".concat(student.full_name, " can't join."));
-        }
-    };
-    ;
-    Course.prototype.remove_student = function (student) {
-        if (this.students.includes(student)) {
-            this.students = this.students.filter(function (el) { return el.full_name !== student.full_name; });
-            console.log("".concat(student.full_name, " has been removed from course ").concat(this.title, "."));
-        }
-        else {
-            console.log("".concat(student.full_name, " not found in course ").concat(this.title, "."));
-        }
-    };
-    ;
-    return Course;
-}());
-var Professor = /** @class */ (function () {
-    function Professor(name, address, phone_number, email, salary) {
-        this.name = name;
-        this.address = address;
-        this.email = email;
-        this.phone_number = phone_number;
+        this.phoneNumber = phoneNumber;
+        this.position = position;
+        this.rank = rank;
         this.salary = salary;
     }
-    ;
-    Professor.prototype.check_assignment = function (obj) {
-        if (obj.is_done) {
-            console.log("Assignment is done. You can get your mark: 5.");
+    return PersonalInfo;
+}());
+var Staff = /** @class */ (function () {
+    function Staff(id, fullName, address, phoneNumber, email, position, rank, salary) {
+        this.personalInfo = new PersonalInfo(id, fullName, address, phoneNumber, email, position, rank, salary);
+    }
+    return Staff;
+}());
+var PostgraduateStudent = /** @class */ (function (_super) {
+    __extends(PostgraduateStudent, _super);
+    function PostgraduateStudent(id, fullName, address, phoneNumber, email, position, rank, salary, phdStatus) {
+        var _this = _super.call(this, id, fullName, address, phoneNumber, email, position, rank, salary) || this;
+        _this.phdStatus = phdStatus;
+        return _this;
+    }
+    PostgraduateStudent.prototype.askSickLeave = function (department) {
+        return false;
+    };
+    PostgraduateStudent.prototype.sendRequest = function (department) {
+        return true;
+    };
+    return PostgraduateStudent;
+}(Staff));
+var Student = /** @class */ (function (_super) {
+    __extends(Student, _super);
+    function Student(id, fullName, address, phoneNumber, email, position, rank, salary) {
+        return _super.call(this, id, fullName, address, phoneNumber, email, position, rank, salary) || this;
+    }
+    Student.prototype.askSickLeave = function (department) {
+        return false;
+    };
+    Student.prototype.sendRequest = function (department) {
+        return true;
+    };
+    return Student;
+}(Staff));
+var Professor = /** @class */ (function (_super) {
+    __extends(Professor, _super);
+    function Professor(id, fullName, address, phoneNumber, email, position, rank, salary) {
+        return _super.call(this, id, fullName, address, phoneNumber, email, position, rank, salary) || this;
+    }
+    Professor.prototype.askSickLeave = function (destination) {
+        return false;
+    };
+    Professor.prototype.sendRequest = function (department) {
+        return true;
+    };
+    Professor.prototype.add_postgraduate_student = function (student) {
+    };
+    return Professor;
+}(Staff));
+var Department = /** @class */ (function () {
+    function Department() {
+    }
+    return Department;
+}());
+var Course = /** @class */ (function () {
+    function Course(title) {
+        this.seminars = [];
+        this.title = title;
+    }
+    return Course;
+}());
+var Seminar = /** @class */ (function () {
+    function Seminar() {
+    }
+    Seminar.prototype.implementItem = function (itemName) {
+        return "str";
+    };
+    Seminar.prototype.addComment = function (str) {
+        console.log("Coment added");
+    };
+    return Seminar;
+}());
+var Enroll = /** @class */ (function () {
+    function Enroll() {
+        this.studentCourses = {};
+        this.courseStudents = {};
+    }
+    Enroll.prototype.enroll = function (student, course) {
+        var keys = Object.keys(this.studentCourses);
+        if (keys.includes(student.personalInfo.id.toString())) {
+            console.log("Currently enroll this course", this.studentCourses);
         }
         else {
-            console.log("Assignment isn't done. You can't get your mark.");
+            this.studentCourses[student.personalInfo.id] = [course.title];
+            this.courseStudents[course.title] = [student.personalInfo.id];
+            //console.log("Welcome new student to this course", this.studentCourses, this.courseStudents)
         }
     };
-    ;
-    return Professor;
+    Enroll.prototype.unenroll = function (student, course) {
+        var keys = Object.keys(this.studentCourses);
+        if (!keys.includes(student.personalInfo.id.toString())) {
+            console.log("There is no such student in this course", this.studentCourses);
+        }
+        else {
+            console.log("Removed student from this course", this.studentCourses);
+            delete this.studentCourses[student.personalInfo.id];
+            delete this.courseStudents[course.title];
+            console.log(this.studentCourses);
+        }
+    };
+    return Enroll;
 }());
-var CourseProgress = /** @class */ (function () {
-    function CourseProgress(received_marks, visited_lectures, completed_assignments, notes) {
-        this.received_marks = received_marks;
-        this.visited_lectures = visited_lectures;
-        this.completed_assignments = completed_assignments;
-        this.notes = notes;
-    }
-    CourseProgress.prototype.get_progress_to_date = function (date) {
-        console.log("Note: ".concat(this.notes[date.toString()]));
-        return this.notes[date.toString()];
-    };
-    ;
-    CourseProgress.prototype.get_final_mark = function () {
-        return 123;
-    };
-    ;
-    CourseProgress.prototype.fill_notes = function (date, note) {
-        //Date can't be used as key;
-        this.notes[date.toString()] = note;
-    };
-    ;
-    CourseProgress.prototype.remove_note = function (date) {
-        delete this.notes[date.toString()];
-    };
-    ;
-    return CourseProgress;
-}());
-//create 2 students
-var OlegStudent = new Student("Oleg Muz", "Doroshenka 50", "0971275232", "olehmuz87@gmail.com", 999, 11.5, []);
-var OlegStudent2 = new Student("Oleg Muz2", "Doroshenka 50", "0971275232", "olehmuz87@gmail.com", 999, 11.5, []);
-// create professor
-var AndriyProfessor = new Professor("Andriy", "Tarnavskogo", "someProfessor@gmail.com", "0971111111", 19500);
-// create course
-var DesignPatterns = new Course("Design Patterns", new Date('Septemper 1, 2022'), new Date('December 1, 2022'), "description", ["1", "2", "3"], ["1", "2", "3"], 1, []);
-//create DesignPatterns Progress and check methods of it;
-var DesignPatternsProgress = new CourseProgress({}, 0, {}, {});
-//create new note
-DesignPatternsProgress.fill_notes(new Date("sep 14, 2022"), "1231");
-console.log(DesignPatternsProgress);
-//remove this note
-DesignPatternsProgress.remove_note(new Date("sep 14, 2022"));
-console.log(DesignPatternsProgress);
-//check methods of Course class
-//add new student to course
-DesignPatterns.add_student(OlegStudent);
-console.log(DesignPatterns);
-//add another one, but limit 1 stop adding;
-DesignPatterns.add_student(OlegStudent2);
-//limit test
-DesignPatterns.add_student(OlegStudent2);
-//removing of student
-DesignPatterns.remove_student(OlegStudent);
-console.log(DesignPatterns);
-//Student methods
-OlegStudent2.can_enroll(DesignPatterns);
-OlegStudent2.enroll(DesignPatterns);
-console.log(OlegStudent2);
-OlegStudent2.unenroll(DesignPatterns);
-console.log(OlegStudent2);
-// node . in patterns folder to start script
+var PostStud = new PostgraduateStudent(4, "Oleg MUz", "Doroshenka 50", "+380971275232", "olehmuz87@gmail.com", "full-stack developer", "rank", 400, "Magistr");
+var stud1 = new Student(3, "Oleg MUz", "Doroshenka 50", "+380971275232", "olehmuz87@gmail.com", "full-stack developer", "rank", 400);
+var stud = new Student(4, "Oleg MUz", "Doroshenka 50", "+380971275232", "olehmuz87@gmail.com", "full-stack developer", "rank", 400);
+var enroll = new Enroll();
+var course = new Course("Patterns");
+enroll.enroll(stud, course);
+console.log(enroll);
+enroll.enroll(stud, course);
+enroll.enroll(stud1, course);
+enroll.unenroll(stud, course);
+console.log(enroll);
--- index.ts
+++ index.ts
@@ -1,248 +1,258 @@
-class Student {
-  full_name: string;
+class PersonalInfo {
+  fullName: string;
   address: string;
-  phone_number: string;
+  phoneNumber: string;
   email: string;
-  student_number: number;
-  average_mark: number;
-  courses: Course[];
-  can_enroll(course: Course): boolean{
-    if(course.students.includes(this) && this.courses.includes(course)){
-      console.log(`${this.full_name} may enroll.`);
-      return true;
-    }
-    console.log(`${this.full_name} can't enroll.`);
-    return false;
-  };
-  taken_courses(): Course[]{
-    console.log(`Student courses: ${this.courses}`);
-    return this.courses;
-  };
-  enroll(course: Course): void{
-    if(this.courses.includes(course)){
-      console.log(`${this.full_name} currently enroll this course`);
-    }
-    else{
-      this.courses = [...this.courses, course];
-      console.log(`${this.full_name} enrolled this course`);
-    }
-  };
-  unenroll(course: Course): void{
-    if(!this.courses.includes(course)){
-      console.log(`${this.full_name} currently unenrolled this course`);
-    }
-    else{
-      this.courses = this.courses.filter(el => el.title != course.title);
-      console.log(`${this.full_name} unenrolled this course`);
-    }
-  };
-
+  id: number;
+  position: number;
+  rank: string;
+  salary: number;
   constructor(
-    full_name: string,
-    address: string,
-    phone_number: string,
-    email: string,
-    student_number: number,
-    average_mark: number,
-    courses: []
+    id,
+    fullName,
+    address,
+    phoneNumber,
+    email,
+    position,
+    rank,
+    salary
   ) {
-    this.full_name = full_name;
+    this.id = id;
+    this.fullName = fullName;
     this.address = address;
     this.email = email;
-    this.student_number = student_number;
-    this.phone_number = phone_number;
-    this.average_mark = average_mark;
-    this.courses = courses;
+    this.phoneNumber = phoneNumber;
+    this.position = position;
+    this.rank = rank;
+    this.salary = salary;
   }
 }
 
-type assignment = {
-  title: string;
-  is_done: boolean;
-  description: string;
-  mark: number;
-};
+abstract class Staff {
+  personalInfo: PersonalInfo;
+  constructor(
+    id,
+    fullName,
+    address,
+    phoneNumber,
+    email,
+    position,
+    rank,
+    salary){
+      this.personalInfo = new PersonalInfo(id,
+        fullName,
+        address,
+        phoneNumber,
+        email,
+        position,
+        rank,
+        salary)
+    }
+  abstract askSickLeave(department: Department): boolean
+  abstract sendRequest(department: Department): boolean
+}
+class PostgraduateStudent extends Staff {
+  askSickLeave(department: Department): boolean {
+    return false;
+  }
+  sendRequest(department: Department): boolean {
+    return true;
+  }
+  phdStatus: string;
+
+
+  constructor(
+    id,
+    fullName,
+    address,
+    phoneNumber,
+    email,
+    position,
+    rank,
+    salary,
+    phdStatus
+  ) {
+    super(id,
+      fullName,
+      address,
+      phoneNumber,
+      email,
+      position,
+      rank,
+      salary);
+    this.phdStatus = phdStatus;
+    
+  }
+  
+  
+}
+class Student extends Staff{
+  askSickLeave(department: Department): boolean {
+    return false;
+  }
+  sendRequest(department: Department): boolean {
+    return true;
+  }
+
+  constructor(
+    id,
+    fullName,
+    address,
+    phoneNumber,
+    email,
+    position,
+    rank,
+    salary
+  ) {
+    super(id,
+      fullName,
+      address,
+      phoneNumber,
+      email,
+      position,
+      rank,
+      salary);
+    
+  }
+}
+class Professor extends Staff {
+  askSickLeave(destination: Department): boolean {
+    return false;
+  }
+  sendRequest(department: Department): boolean {
+    return true;
+  }
+  add_postgraduate_student(student: PostgraduateStudent): void {
+
+  }
+
+  constructor(
+    id,
+    fullName,
+    address,
+    phoneNumber,
+    email,
+    position,
+    rank,
+    salary
+  ) {
+    super(id,
+      fullName,
+      address,
+      phoneNumber,
+      email,
+      position,
+      rank,
+      salary);
+    
+  }
+}
+
+class Department {
+  title:string;
+  students:Student[];
+  professors:Professor[];
+  courses:string[];
+  requests:[];
+  proceedRequests():void{
+    console.log('proceedRequests')
+  }
+}
 
 class Course {
-  title: string;
-  start_date: Date;
-  end_date: Date;
-  description: string;
-  lectures: string[];
-  assignments: string[];
-  limit: number;
-  students: Student[];
-  constructor(
-    title: string,
-    start_date: Date,
-    end_date: Date,
-    description: string,
-    lectures: string[],
-    assignments: string[],
-    limit: number,
-    students: Student[]
-  ) {
+  seminars:number[];
+  title: string
+  constructor(title: string){
+    this.seminars = [];
     this.title = title;
-    this.start_date = start_date;
-    this.end_date = end_date;
-    this.description = description;
-    this.lectures = lectures;
-    this.assignments = assignments;
-    this.limit = limit;
-    this.students = students;
-  }
-  add_student(student: Student):void {
-    if(this.students.length + 1 <= this.limit){
-      if(this.students.includes(student)){
-        console.log(`This student already has been added`);
-      }
-      else{
-        this.students = [...this.students, student];
-      }
-    }
-    else{
-      console.log(`The student limit for this course has been exceeded. ${student.full_name} can't join.`);
-    }
-  };
-  remove_student(student: Student):void {
-    if(this.students.includes(student)){
-      this.students = this.students.filter(el => el.full_name !== student.full_name);
-      console.log(`${student.full_name} has been removed from course ${this.title}.`);
-    }
-    else{
-      console.log(`${student.full_name} not found in course ${this.title}.`);
-    }
-  };
-}
-
-class Professor {
-  name: string;
-  address: string;
-  phone_number: string;
-  email: string;
-  salary: number;
-  
-  constructor(
-    name: string,
-    address: string,
-    phone_number: string,
-    email: string,
-    salary: number
-  ) {
-    this.name = name;
-    this.address = address;
-    this.email = email;
-    this.phone_number = phone_number;
-    this.salary = salary;
-  };
-  check_assignment(obj: assignment){
-    if(obj.is_done){
-      console.log(`Assignment is done. You can get your mark: 5.`);
-    }
-    else{
-      console.log(`Assignment isn't done. You can't get your mark.`);
-    }
-  };
-}
-
-class CourseProgress {
-  received_marks: {};
-  visited_lectures: number;
-  completed_assignments: {} | { datetime: assignment };
-  notes: {};
-  get_progress_to_date(date: Date) : string {
-    console.log(`Note: ${this.notes[date.toString()]}`);
-    return this.notes[date.toString()];
-  };
-  get_final_mark():number {
-    return 123;
-  };
-  fill_notes(date: Date,note: string):void {
-    //Date can't be used as key;
-    this.notes[date.toString()] = note;
-  };
-  remove_note(date: Date):void {
-    delete this.notes[date.toString()]
-  };
-  constructor(
-    received_marks: {},
-    visited_lectures: number,
-    completed_assignments: {} | { datetime: assignment },
-    notes: {} | { datetime: string }
-  ) {
-    this.received_marks = received_marks;
-    this.visited_lectures = visited_lectures;
-    this.completed_assignments = completed_assignments;
-    this.notes = notes;
   }
 }
-//create 2 students
-const OlegStudent = new Student(
-  "Oleg Muz",
+class Seminar {
+  id:number;
+  title:string;
+  deadline:Date;
+  assignments:{}[];
+  status: any;
+  related_course:string;
+
+  implementItem(itemName:string):string{
+    return "str";
+  }
+  addComment(str:string): void{
+    console.log("Coment added")
+  }
+}
+class Enroll{
+  studentCourses: {};
+  courseStudents: {};
+  constructor(){
+    this.studentCourses = {};
+    this.courseStudents = {};
+  }
+  enroll(student: Student, course: Course):void{
+    let keys = Object.keys(this.studentCourses);
+    if(keys.includes(student.personalInfo.id.toString())){
+      console.log("Currently enroll this course", this.studentCourses);
+    }
+    else{
+      this.studentCourses[student.personalInfo.id] = [course.title];
+      this.courseStudents[course.title] = [student.personalInfo.id];
+      //console.log("Welcome new student to this course", this.studentCourses, this.courseStudents)
+    }
+  }
+  unenroll(student: Student, course: Course):void{
+    let keys = Object.keys(this.studentCourses);
+    if(!keys.includes(student.personalInfo.id.toString())){
+      console.log("There is no such student in this course", this.studentCourses);
+    }
+    else{
+      console.log("Removed student from this course", this.studentCourses)
+      delete this.studentCourses[student.personalInfo.id];
+      delete this.courseStudents[course.title];
+      console.log(this.studentCourses)
+    }
+  }
+}
+
+const PostStud = new PostgraduateStudent(
+  4,
+  "Oleg MUz",
   "Doroshenka 50",
-  "0971275232",
+  "+380971275232",
   "olehmuz87@gmail.com",
-  999,
-  11.5,
-  []
-);
+  "full-stack developer",
+  "rank",
+  400,
+  "Magistr"
 
-const OlegStudent2 = new Student(
-  "Oleg Muz2",
+)
+
+const stud1 = new Student(
+  3,
+  "Oleg MUz",
   "Doroshenka 50",
-  "0971275232",
+  "+380971275232",
   "olehmuz87@gmail.com",
-  999,
-  11.5,
-  []
-);
-// create professor
-const AndriyProfessor = new Professor(
-  "Andriy",
-  "Tarnavskogo",
-  "someProfessor@gmail.com",
-  "0971111111",
-  19500
-);
-// create course
-const DesignPatterns = new Course(
-  "Design Patterns",
-  new Date('Septemper 1, 2022'),
-  new Date('December 1, 2022'),
-  "description",
-  ["1", "2", "3"],
-  ["1", "2", "3"],
-  1,
-  []
-);
-//create DesignPatterns Progress and check methods of it;
-const DesignPatternsProgress = new CourseProgress({}, 0, {}, {});
-//create new note
-DesignPatternsProgress.fill_notes(new Date("sep 14, 2022"), "1231");
-console.log(DesignPatternsProgress)
-//remove this note
-DesignPatternsProgress.remove_note(new Date("sep 14, 2022"))
-console.log(DesignPatternsProgress)
+  "full-stack developer",
+  "rank",
+  400)
 
+const stud = new Student(
+  4,
+  "Oleg MUz",
+  "Doroshenka 50",
+  "+380971275232",
+  "olehmuz87@gmail.com",
+  "full-stack developer",
+  "rank",
+  400)
 
-//check methods of Course class
-//add new student to course
-DesignPatterns.add_student(OlegStudent)
-console.log(DesignPatterns);
-//add another one, but limit 1 stop adding;
-DesignPatterns.add_student(OlegStudent2)
-//limit test
-DesignPatterns.add_student(OlegStudent2)
-//removing of student
-DesignPatterns.remove_student(OlegStudent)
-console.log(DesignPatterns);
-
-//Student methods
-OlegStudent2.can_enroll(DesignPatterns);
-OlegStudent2.enroll(DesignPatterns)
-console.log(OlegStudent2)
-OlegStudent2.unenroll(DesignPatterns)
-console.log(OlegStudent2)
-
-
-// node . in patterns folder to start script
+const enroll = new Enroll();
+const course = new Course("Patterns");
+enroll.enroll(stud, course);
+console.log(enroll);
+enroll.enroll(stud, course);
+enroll.enroll(stud1, course);
+enroll.unenroll(stud, course);
+console.log(enroll);
+ 
